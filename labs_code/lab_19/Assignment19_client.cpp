aa71c7a842cb88d875eb925122f723c82c37a65a
ShumwayGordon <33491221+ShumwayGordon@users.noreply.github.com>
2022-06-29 19:05:54 +0300

    Cleaned code

--- labs_code/lab_19/Assignment19_client.cpp
+++ labs_code/lab_19/Assignment19_client.cpp
@@ -1,4 +1,3 @@
-/*
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
@@ -15,18 +14,14 @@ int main(int argc, char **argv)
 	strcpy(port_name, argv[1]);
 
 	MPI_Comm_connect(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
-	//printf("Client connected\n");
 	cout << "Client connected" << endl;
 
 	MPI_Send(&message, 1, MPI_INT, 0, 0, intercomm);
-	//printf("Client sent: %d\n", message);
 	cout << "Client sent: " << message << endl;
 
 	MPI_Recv(&recv_msg, 1, MPI_INT, 0, 0, intercomm, &status);
-	//printf("Client recieved: %d\n", recv_msg);
 	cout << "Client recieved: " << recv_msg << endl;
 
 	MPI_Finalize();
 	return 0;
 }
-*/
