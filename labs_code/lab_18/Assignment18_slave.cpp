641d04982f58ea97eea3a16e23e83786078c30fa
ShumwayGordon <33491221+ShumwayGordon@users.noreply.github.com>
2022-06-29 19:05:26 +0300

    Cleaned code

--- labs_code/lab_18/Assignment18_slave.cpp
+++ labs_code/lab_18/Assignment18_slave.cpp
@@ -1,4 +1,3 @@
-/*
 #include "mpi.h"
 int main(int argc, char **argv)
 {
@@ -17,29 +16,3 @@ int main(int argc, char **argv)
 	MPI_Finalize();
 	return 0;
 }
-*/
-
-/*
-#include "mpi.h"
-int main(int argc, char** argv)
-{
-	int rank;
-	MPI_Comm intercomm;
-	MPI_Init(&argc, &argv);
-	// Get parent communicator
-	MPI_Comm_get_parent(&intercomm);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	if (rank < 2)
-		// Send rank to parent
-		MPI_Send(&rank, 1, MPI_INT, 0, rank, intercomm);
-	else
-	{
-		//Send slaves number to parent
-		int size;
-		MPI_Comm_size(MPI_COMM_WORLD, &size);
-		MPI_Send(&size, 1, MPI_INT, 0, rank, intercomm);
-	}
-	MPI_Finalize();
-	return 0;
-}
-*/
