44510b7784188bb7b0d7d5d30d397991d65629c5
ShumwayGordon <33491221+ShumwayGordon@users.noreply.github.com>
2022-06-29 17:41:02 +0300

    Added labs code

---
+++ labs_code/lab_12/Assignment12.cpp
@@ -0,0 +1,37 @@
+/*
+#include <iostream>
+#include "mpi.h"
+
+int main(int argc, char** argv)
+{
+	int rank, size, prev, next;
+	float rbuf[2], sbuf[2];
+	MPI_Init(&argc, &argv);
+	MPI_Request reqs[4];
+	MPI_Status stats[4];
+
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	prev = rank - 1;
+	next = rank + 1;
+	if (rank == 0) prev = size - 1;
+	if (rank == size - 1) next = 0;
+	MPI_Recv_init(&rbuf[0], 1, MPI_FLOAT, prev, 5, MPI_COMM_WORLD, &reqs[0]);
+	MPI_Recv_init(&rbuf[1], 1, MPI_FLOAT, next, 6, MPI_COMM_WORLD, &reqs[1]);
+	MPI_Send_init(&sbuf[0], 1, MPI_FLOAT, prev, 6, MPI_COMM_WORLD, &reqs[2]);
+	MPI_Send_init(&sbuf[1], 1, MPI_FLOAT, next, 5, MPI_COMM_WORLD, &reqs[3]);
+	
+	for (int i = 0; i < 2; i++) 
+		sbuf[i] = rank;
+
+	MPI_Startall(4, reqs);
+	MPI_Waitall(4, reqs, stats);
+	for(int i = 0; i < 4; i++)
+		MPI_Request_free(&reqs[i]);
+
+	printf("Process #%d - prev_mess = %f, next_mess = %f\n",
+		   rank, rbuf[0], rbuf[1]);
+
+	MPI_Finalize();
+}
+*/
---
+++ labs_code/lab_7/Assignment7.cpp
@@ -0,0 +1,71 @@
+/*
+#include <iostream>
+#include <mpi.h>
+using namespace std;
+
+int main( int argc, char** argv )
+{
+	int rank, size, part_size, arr_len;
+	int root_proc = 0;
+	if (argc == 1)
+	{
+		printf("TEST MODE\n\n");
+		arr_len = 1000000;
+	}
+	else if (argc == 2)
+		arr_len = atoi(argv[1]);
+	else
+	{
+		printf("Wrong number of parameters. Must be 1.");
+		return 1;
+	}
+	
+	MPI_Init(&argc, &argv);
+	int *arr_1 = nullptr;
+	int *arr_2 = nullptr;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	part_size = arr_len / size;
+
+	if (rank == root_proc)
+	{
+		arr_1 = (int*)malloc(sizeof(int) * arr_len);
+		arr_2 = (int*)malloc(sizeof(int) * arr_len);
+
+		for (int i = 0; i < arr_len; i++)
+		{
+			arr_1[i] = 1;
+			arr_2[i] = 1;
+		}
+	}
+
+	int* part_arr_1 = (int*)malloc(sizeof(int) * part_size);
+	int* part_arr_2 = (int*)malloc(sizeof(int) * part_size);
+
+	MPI_Scatter(arr_1, part_size, MPI_INT, part_arr_1, part_size, MPI_INT, root_proc, MPI_COMM_WORLD);
+	MPI_Scatter(arr_2, part_size, MPI_INT, part_arr_2, part_size, MPI_INT, root_proc, MPI_COMM_WORLD);
+
+	int loc_sum = 0;
+	for(int i = 0; i < part_size; i++)
+		loc_sum += part_arr_1[i] * part_arr_2[i];
+
+	int sum;
+
+	MPI_Reduce(&loc_sum, &sum, 1, MPI_INT, MPI_SUM, root_proc, MPI_COMM_WORLD);
+
+	if (rank == root_proc)
+		printf("Dot product = %i\n", sum);
+
+	free(part_arr_1);
+	free(part_arr_2);
+	if (rank == root_proc)
+	{
+		free(arr_1);
+		free(arr_2);
+	}
+
+	MPI_Finalize();
+	return 0;
+}
+*/
